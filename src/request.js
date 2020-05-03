20a4d9c7107ee3d2d4c019dd0bca121189526a40
Tom <tom@fourzerofour.pw>
2020-05-03 16:53:25 +1000

    Modularity improved

--- src/request.js
+++ src/request.js
@@ -1,14 +1,21 @@
 const tls = require('tls');
 const url = require('url');
 
-const errorCodes = require('./codes.js');
+const statusCodes = require('./codes.js');
 
+/**
+ * Requests content via the Gemini protocol.
+ *
+ * @param {url.URL} address -- Of the form URL('gemini://host.name/path')
+ * @param {number} timeout -- Connection timeout, in milliseconds
+ * @returns {Promise} -- Either a TimeoutError or Response
+ */
 async function request(address, timeout = 3000) {
 
   const socket = await new Promise((resolve, reject) => {
     const socket = tls.connect({
       host: address.host,
-      port: address.port || 1965,
+      port: 1965,
     }, () => resolve(socket));
     socket.setTimeout(timeout, () => {
       socket.destroy();
@@ -20,34 +27,32 @@ async function request(address, timeout = 3000) {
     socket.write(`${address.href}\r\n`, 'utf8');
     socket.once('data', (data) => {
       data = String(data);
-      const statusCode = data.slice(0, 2);
+      const code = Number(data.slice(0, 2));
       const metaRegExp = /^\d\d\s+(.+)\r\n$/;
       const matches = data.match(metaRegExp);
       const meta = matches && matches[1];
       const result = {
-        statusCode,
+        status: statusCodes.filter(c => c.code === code)[0],
         meta,
       };
-      if (statusCode[0] !== '2') reject(result);
-      else resolve(result);
+      if (result.status.isOk) resolve(result);
+      else reject(result);
     });
   });
 
   try {
     await header;
-    const body = await new Promise((resolve, reject) => {
-      let buffer = Buffer.alloc(0);
-      socket.on('data', (data) => buffer = Buffer.concat([buffer, data]));
-      socket.on('end', () => resolve(buffer));
-    });
-    return { address, header: await header, body };
   } catch (error) {
-    if (error.statusCode[0] === '3') {
-      const redirectAddress = new url.URL(error.meta);
-      return request(redirectAddress);
-    }
-    throw error;
+    throw { address, header: error, body: null };
   }
+
+  const body = await new Promise((resolve, reject) => {
+    let buffer = Buffer.alloc(0);
+    socket.on('data', (data) => buffer = Buffer.concat([buffer, data]));
+    socket.on('end', () => resolve(buffer));
+  });
+
+  return { address, header: await header, body };
 
 };
 
@@ -55,31 +60,15 @@ async function request(address, timeout = 3000) {
 module.exports = request;
 
 
+// Testing
 if (require.main === module) {
 
   const args = process.argv.slice(2);
 
-  if (!args.length) {
-    console.error('No URL specified!');
-    process.exit(-1);
-  }
-
   const address = new url.URL(args[0]);
 
   request(address)
-    .then((response) => {
-      if (response.header.meta.indexOf('text') !== -1) {
-        console.log(response.body.toString());
-      }
-    })
-    .catch((error) => {
-      if (error.statusCode) {
-        const relevantCode = errorCodes.filter(c => c.code === error.statusCode)[0];
-        console.error(relevantCode.code, relevantCode.message);
-        console.error(relevantCode.description.split('\n').join(' ').split('\t').join(' '));
-        return;
-      }
-      console.error(error);
-    });
+    .then(console.log)
+    .catch((e) => console.error(JSON.stringify(e, null, 2)));
 
 }
